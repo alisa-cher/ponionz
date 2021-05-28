add25e12dfc63daee12a842f4402ed33d9f916e3
Alisa Chernysheva <chernysheva.alisa@gmail.com>
2021-05-28 11:51:57 +0200

    Initial commit

---
+++ src/main/java/fr/simplon/ponionz/PonionzApplication.java
@@ -0,0 +1,13 @@
+package fr.simplon.ponionz;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PonionzApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PonionzApplication.class, args);
+    }
+
+}
